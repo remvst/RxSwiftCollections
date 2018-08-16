a536e3270df5acf39877d72b5104ec3d2781f8af
Mike Roberts <mike@kik.com>
2018-08-16 12:48:28 -0400

    Wrap index list lookup

--- RxSwiftCollections/Classes/IndexedObservableList.swift
+++ RxSwiftCollections/Classes/IndexedObservableList.swift
@@ -8,6 +8,64 @@
 import Foundation
 import RxSwift
 
+class Index<T> {
+    let value: Int
+    private weak var internalPrevious: PublishSubject<T?>?
+    private weak var internalNext: PublishSubject<T?>?
+    
+    public var previous: PublishSubject<T?> {
+        get {
+            let previous = internalPrevious ?? PublishSubject<T?>()
+            
+            internalPrevious = previous
+            
+            return previous
+        }
+    }
+    
+    public var next: PublishSubject<T?> {
+        get {
+            let next = internalNext ?? PublishSubject<T?>()
+            
+            internalNext = next
+            
+            return next
+        }
+    }
+    
+    init(value: Int) {
+        self.value = value
+    }
+}
+
+class Indices<T>: NSObject {
+    private var indices: [Index<T>]
+    
+    override init() {
+        self.indices = []
+    }
+    
+    func get(_ index: Int) -> Index<T> {
+        return Index<T>(value: index)
+    }
+}
+
+class UpdateHolder<T>: NSObject {
+    let indices: Indices<T>
+    let update: Update<T>?
+    
+    override convenience init() {
+        self.init(indices: Indices<T>(), update: nil)
+    }
+    
+    init(indices: Indices<T>, update: Update<T>?) {
+        self.indices = indices
+        self.update = update
+        
+        super.init()
+    }
+}
+
 private class IndexedObservableList<T, U>: ObservableList<U> {
     private let list: ObservableList<T>
     private let transform: ((T, Observable<T?>, Observable<T?>) -> U)
@@ -21,9 +79,19 @@ private class IndexedObservableList<T, U>: ObservableList<U> {
         let transform = self.transform
         
         return list.updates
-            .map { Update(list: $0.list.lazy.map({ value in
-                transform(value, Observable.empty(), Observable.empty())
-            }), changes: $0.changes) }
+            .scan(UpdateHolder<T>()) { (holder, update) -> UpdateHolder<T> in
+                return UpdateHolder(indices: holder.indices, update: update)
+            }
+            .map { holder in
+                let placeholderList = 0..<holder.update!.list.count
+                
+                return Update(list: placeholderList.lazy.map({ index in
+                    let value = holder.update!.list[index]
+                    let indexWrapper = holder.indices.get(index)
+                    
+                    return transform(value, indexWrapper.previous, indexWrapper.next)
+                }), changes: holder.update!.changes)
+            }
     }
 }
 
